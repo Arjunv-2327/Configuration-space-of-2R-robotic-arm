6cca4ccb68c4dcd378b8a004b631ef467be3e66c
Arjun V <write2ananthanarayanan@gmail.com>
2025-05-30 15:44:20 +0530

    Update main_script.m

--- Vertex_Collision_Approach/Helper_Functions/main_script.m
+++ Vertex_Collision_Approach/Helper_Functions/main_script.m
@@ -3,10 +3,26 @@ l = 1.2;               % Length
 w = 0.6;               % Width
 alpha = -0.9;          % Center X
 beta = 0.9;            % Center Y
-phi = 0;        % Inclination
-dw = -70/3;               % Deviation in degrees from theta1 limit
+phi = 0.820305;        % Inclination
 
+
+% Robot link parameters
+L_1 = 3; % Length of link 1
+L_2 = 3; % Length of link 2
+dw = 12*3; % Deviation from the safe limit for theta_1
+
+
+% Obstacle creation
 obstacle = create_obstacle(l, w, alpha, beta, phi);
-[theta1, theta2] = detectAngularCollision(3, obstacle);
-[blue_angle, magenta_angle] = safespace(r, obstacle);
-theta2collisionrange(magenta_angle, dw, obstacle); % enter theta1 limit point argument, and deviation in degrees
+
+
+% Collision limits for link 1
+[theta1, theta2] = detectAngularCollision(L_1, obstacle);
+
+
+% Safe region, both links do not collide - entire robot is safe
+[blue_angle, magenta_angle] = safespace(L_1, L_2, obstacle);
+
+
+% Get the range of unsafe range of theta_2 after deviating link 1
+theta2collisionrange(magenta_angle, dw, obstacle, L_2); 
